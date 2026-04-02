722d52059f6ffc5d7ed547c0b9a016b025ca10c8
Raul Menezes <raul@rmenezes.me>
2026-04-03 00:16:55 +0100

    Bump version to 0.1.0 and propagate to subprojects
    Ensures plugin marker artifacts are published with the correct version
    instead of 'unspecified'.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 group = "dev.anicanon.swiftjava"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 allprojects {
+    group = rootProject.group
+    version = rootProject.version
     repositories {
         mavenCentral()
         google()
